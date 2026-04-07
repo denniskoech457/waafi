ab92ccae87490bdd8daef3b1617201d4d3e345d8
Dennis Koech <73883136+denniskoech457@users.noreply.github.com>
2026-04-07 22:29:40 +0300

    Delete send-message.php

--- send-message.php
+++
@@ -1,6 +0,0 @@
-<form action="send-telegram.php" method="POST">
-  <input type="text" name="name" placeholder="Your name" required>
-  <input type="email" name="email" placeholder="Your email" required>
-  <textarea name="message" placeholder="Your message" required></textarea>
-  <button type="submit">Send</button>
-</form>
